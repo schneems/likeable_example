9c43477a04205ada37eabf7c5060b98837ef851f
Carlos A. da Silva <carlosantoniodasilva@gmail.com>
2009-10-08 19:58:58 -0300

    Removing devise tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
-#require 'webrat'
-#require 'webrat/core/matchers'
-require 'integration_tests_helper'
 
-Webrat.configure do |config|
-  config.mode = :rails
-end
+#Webrat.configure do |config|
+#  config.mode = :rails
+#end
 
 class ActiveSupport::TestCase
 
