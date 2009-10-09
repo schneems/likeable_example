8a5cfa8810f7f492eb4b2f37ac8c1fcda14e1e05
Carlos A. da Silva <carlosantoniodasilva@gmail.com>
2009-10-09 07:39:41 -0300

    Adding user controller for editing email and password.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  before_filter :authenticate!
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(params[:user])
+      flash[:notice] = 'Updated successfully'
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resources :home, :only => :index
+  map.resource :user, :only => [:edit, :update]
 
   map.root :controller => :home
 end
