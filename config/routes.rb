15488d23194780258b97dac8b4fc589a9b422db0
Carlos A. da Silva <carlosantoniodasilva@gmail.com>
2009-10-13 17:00:20 -0300

    Fix routes, add docs about footnotes and fix layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.devise_for :user, :admin
+  map.devise_for :users, :admin
 
   map.resources :home, :only => :index
   map.resource :user, :only => [:edit, :update]
